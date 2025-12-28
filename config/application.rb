5142c0f5a00e8560dee77b2398ba91e719131b8c
ctneal91 <ctnealherman@gmail.com>
2025-12-28 16:05:51 -0600

    Enable bcrypt and add session middleware for authentication

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,12 @@ module Login
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add back session and cookie middleware for session-based authentication
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: "_myapp_session",
+      same_site: :lax,
+      secure: Rails.env.production?
   end
 end
